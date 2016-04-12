76e145200d054a9d5ffd1f23cceaf83e3b85bc8b
pilotpirx1 <pawel.gielmuda@gmail.com>
2016-04-12 07:57:04 +0200

    Score missing item

--- Savior Beginnings - Unity5.3.2/Assets/Scripts/ScoreControler.cs
+++ Savior Beginnings - Unity5.3.2/Assets/Scripts/ScoreControler.cs
@@ -1,27 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
+using UnityEngine.UI;
 
 public class ScoreControler : MonoBehaviour
 {
+  public float Score;
+
   private static ScoreControler _controler;
+  private bool UpdateNeeded;
+  private Text text;
+
   public static ScoreControler Get()
   {
     return _controler;
   }
-	// Use this for initialization
-	void Start ()
-	{
-	  _controler = this;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+  // Use this for initialization
+  void Start()
+  {
+    _controler = this;
+    Score = 0;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (UpdateNeeded)
+    {
+      text.text = string.Format("Score: {0:F0}", Score);
+    }
+  }
 
   internal void AddScore(float increment)
   {
-    throw new NotImplementedException();
+    UpdateNeeded = true;
+    Score += increment;
   }
 }
